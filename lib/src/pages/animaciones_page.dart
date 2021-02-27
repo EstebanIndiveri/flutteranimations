d358c2bc3a7897ed5f30e246a23fd30dbc772fad
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-27 00:25:34 -0300

    change opacity

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -21,15 +21,25 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>with SingleTickerProvi
   AnimationController controller;
   Animation<double>rotation;
   Animation<double>opacity;
+  Animation<double>moveRight;
+  Animation<double>scale;
   @override
   void initState() {
     // TODO: implement initState
     controller=new AnimationController(vsync: this,duration: Duration(milliseconds: 4000));
     rotation=Tween(begin: 0.0,end: 2 * Math.pi).animate(
-      CurvedAnimation(parent: controller,curve: Curves.slowMiddle)
+      CurvedAnimation(parent: controller,curve: Curves.easeOut)
     );
 
-    opacity=Tween(begin: 0.1,end: 1.0).animate(controller);
+    opacity=Tween(begin: 0.1,end: 1.0).animate(
+      CurvedAnimation(parent: controller,curve: Interval(0, 0.25,curve: Curves.easeOut))
+    );
+    moveRight=Tween(begin: 0.0,end:200.0).animate(
+      CurvedAnimation(parent: controller,curve: Curves.easeOut)
+    );
+    scale=Tween(begin: 0.0,end:2.0).animate(
+      CurvedAnimation(parent: controller,curve: Curves.easeOut)
+    );
 
     controller.addListener(() {
       print('status: ${controller.status}');
@@ -59,13 +69,20 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>with SingleTickerProvi
       child: _Rectangulo(),
       builder: (BuildContext context, Widget childRectangulo) {
         // print('rotation'+rotation.value.toString());
-        return Transform.rotate(
-          angle: rotation.value,
-          child: Opacity(
-          opacity: opacity.value,
-          child: childRectangulo,
-          )
-          );
+        return Transform.translate(
+          offset: Offset(moveRight.value,0),
+            child: Transform.rotate(
+            angle: rotation.value,
+            child: Opacity(
+            opacity: opacity.value,
+            child: Transform.scale
+            (
+              scale: scale.value,
+              child: childRectangulo
+            ),
+            )
+            ),
+        );
       },
     );
   }

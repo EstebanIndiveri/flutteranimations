1fbce7bcfd3170e1b2ecb9c725935fb58e38c287
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-27 00:11:21 -0300

    change opacity

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math' as Math;
 
 class AnimacionesPage extends StatelessWidget {
   @override
@@ -20,12 +20,26 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>with SingleTickerProvi
 
   AnimationController controller;
   Animation<double>rotation;
-
+  Animation<double>opacity;
   @override
   void initState() {
     // TODO: implement initState
     controller=new AnimationController(vsync: this,duration: Duration(milliseconds: 4000));
-    rotation=Tween(begin: 0.0,end: 2.0).animate(controller);
+    rotation=Tween(begin: 0.0,end: 2 * Math.pi).animate(
+      CurvedAnimation(parent: controller,curve: Curves.slowMiddle)
+    );
+
+    opacity=Tween(begin: 0.1,end: 1.0).animate(controller);
+
+    controller.addListener(() {
+      print('status: ${controller.status}');
+      if(controller.status==AnimationStatus.completed){
+        // controller.reverse();
+        controller.reset();
+
+      }
+    });
+
     super.initState();
   }
   @override
@@ -42,12 +56,16 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>with SingleTickerProvi
     // return _Rectangulo();
     return AnimatedBuilder(
       animation: controller,
-      // child: child,
-      builder: (BuildContext context, Widget child) {
+      child: _Rectangulo(),
+      builder: (BuildContext context, Widget childRectangulo) {
         // print('rotation'+rotation.value.toString());
         return Transform.rotate(
           angle: rotation.value,
-          child: _Rectangulo());
+          child: Opacity(
+          opacity: opacity.value,
+          child: childRectangulo,
+          )
+          );
       },
     );
   }
